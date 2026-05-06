cf2509353c4907ed8cf459ef2f44651ce0061488
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-06 13:20:52 +0530

    Update UserAPI.js

--- Week 7 BLOG-APP/backend/APIs/UserAPI.js
+++ Week 7 BLOG-APP/backend/APIs/UserAPI.js
@@ -1,34 +1,44 @@
+// create mini applications
 import exp from 'express'
-import { verifyToken } from '../middlewares/VerifyToken.js'
-import { ArticleModel } from '../models/ArticleModel.js'
-export const userApp=exp.Router()
+import {ArticleModel} from '../models/ArticleModel.js'
+import {UserModel} from '../models/UserModel.js'
+import {verifyToken} from '../middlewares/verifyToken.js'
+export const userApp = exp.Router()
 
-
-//Read articles of all authors 
-userApp.get("/articles",verifyToken("USER"),async(req,res)=>{
-//read articles
-const articlesList = ArticleModel.find({isArticleActive:true})
-//send res
-res.status(200).json({message:""})
+// read all article route
+userApp.get('/articles',verifyToken("USER"),async(req,res)=>{
+    // get id of the user
+    const userId = req.user?.id;
+    // find user
+    const user = await UserModel.findById(userId);
+    if(!user.isUserActive){
+        return res.status(400).json({message:"You are blocked from entering further pages"})
+    }
+    // read articles
+    const articlesList = await ArticleModel.find({isArticleActive:true});
+    //send res
+    res.status(200).json({message:"All available Articles",payload:articlesList});
 })
 
-
-//Add comment to an article
-userApp.put("/articles",verifyToken("USER"),async(req,res)=>{
-    //get body from req
+// add a comment
+userApp.put('/articles',verifyToken("USER"),async(req,res)=>{
+    // get req body
+    console.log(req.body);
     const {articleId,comment}=req.body
-    //check article
-    const articleDocument=await ArticleModel.findOne({_id:articleId, isArticleActive:true})
-    //if article not found
-    if(!articleDocument){
-        return res.status(404).json({message:'Article not found'})
+    // find the article
+    
+    const articleDoc = await ArticleModel.findOne({_id:articleId,isArticleActive:true})
+                            .populate("comment.user");
+    if(!articleDoc){
+        return res.status(404).json({message:"Article not found"})
     }
-    //get user id
-    const userId=req.user?.id;
-    //add comments to commments array of articles Document
-    articleDocument.comments.push({user:userId,comment:comment});
-    //save
-    await articleDocument.save();
-    //send res
-    res.status(200).json({message:"comment added Successfully",payload:articleDocument})
+    // find the user
+    const userId = req.user?.id;
+    
+    //add the comment
+    articleDoc.comment.push({user:userId,comment:comment});
+
+    await articleDoc.save();
+    // send res
+    res.status(200).json({message:"Comment added",payload:articleDoc})
 })
