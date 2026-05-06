3c2dc15127ecf7831af4cb2047a300cb68ee1fe9
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-06 13:22:07 +0530

    Update commonAPI.js

--- Week 7 BLOG-APP/backend/APIs/commonAPI.js
+++ Week 7 BLOG-APP/backend/APIs/commonAPI.js
@@ -1,135 +1,162 @@
-import exp from "express";
-import { UserModel } from "../models/UserModel.js";
-import { hash, compare } from "bcryptjs";
-import { config } from "dotenv";
-import jwt from "jsonwebtoken";
-import { verifyToken } from "../middlewares/VerifyToken.js";
-const { sign } = jwt;
-export const commonApp = exp.Router();
-import { upload } from "../config/multer.js";
-import { uploadToCloudinary } from "../config/cloudinaryUpload.js";
-import cloudinary from "../config/cloudinary.js";
-config();
+// create mini applications
+import exp from 'express'
+import {UserModel} from '../models/UserModel.js'
+import {hash,compare} from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { userApp } from './UserAPI.js'
+import { verifyToken } from '../middlewares/verifyToken.js'
 
-//Route for register
-commonApp.post("/users", upload.single("profileImageUrl"), async (req, res) => {
-  let cloudinaryResult;
-  try {
-    let allowedRoles = ["USER", "AUTHOR"];
-    //get user from req
-    const newUser = req.body;
-    console.log(newUser);
-    console.log(req.file);
+export const commonApp = exp.Router()
+import {upload} from '../config/multer.js'
+import {uploadToCloudinary} from '../config/cloudinaryUpload.js'
+import cloudinary from '../config/cloudinary.js'
 
-    //check role
-    if (!allowedRoles.includes(newUser.role)) {
-      return res.status(400).json({ message: "Invalid role" });
+const {sign}=jwt
+
+// Route to register
+commonApp.post('/users', upload.single("profileImageUrl"), async (req, res, next) => {
+    let cloudinaryResult;
+
+    try {
+        // get tthe details of the user
+        const newUser = req.body;
+
+        // check for the roles : only author and user not admin
+        let allowedRoles = ['USER', 'AUTHOR']
+        if (!allowedRoles.includes(newUser.role))
+            return res.status(400).json({ message: "invalid role" })
+
+        /// upload image to cloudinary from memoryStorage
+        if (req.file) {
+    try {
+        const cloudinaryResult = await uploadToCloudinary(req.file.buffer);
+        newUser.profileImageUrl = cloudinaryResult?.secure_url;
+    } catch (err) {
+        newUser.profileImageUrl = "";
+    }
+} else {
+    newUser.profileImageUrl = "";
+}
+
+        // add CDN link of image to new userObj
+        newUser.profileImageUrl = cloudinaryResult?.secure_url;
+
+        // RUN VALIDATORS MANUALLY
+
+        // replace the password eith hashed password
+        newUser.password = await hash(newUser.password, 12)
+
+        // create document
+        const userDocument = new UserModel(newUser);
+
+        // save document
+        await userDocument.save()
+
+        // send respone
+        res.status(201).json({ message: "User registered " });
+
+    } catch (err) {
+        //delete image from cloudinary
+        if (cloudinaryResult?.public_id)
+            await cloudinary.uploader.destroy(cloudinaryResult.public_id)
+
+        next(err);
+    }
+})
+
+// route for login
+commonApp.post('/login', async (req, res) => {
+    // get email and password from the req
+    const { email, password } = req.body;
+
+    // get user details
+    const user = await UserModel.findOne({ email: email })
+    if (!user) {
+        return res.status(400).json({ message: "Invalid email" });
     }
 
-    //Upload image to cloudinary from memoryStorage
-    if (req.file) {
-      cloudinaryResult = await uploadToCloudinary(req.file.buffer);
+    // compare the password with og password
+    let isMatched = await compare(password, user.password)
+    if (!isMatched) {
+        return res.status(400).json({ message: "Incorrect password" });
     }
 
-    // console.log("cloudinaryResult", cloudinaryResult);
-    //add CDN link(secure_url) of image to newUserObj
-    newUser.profileImageUrl = cloudinaryResult?.secure_url;
+    // TOKEN CREATION
+    const signedToken = sign(
+        {
+            id: user._id,
+            email: user.email,
+            role: user.role,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            profileImageUrl: user.profileImageUrl
+        },
+        process.env.KEY,
+        { expiresIn: "1h" }
+    )
 
-    //run validators manually
-    //hash password and replace plain with hashed one
-    newUser.password = await hash(newUser.password, 12);
+    // set token to the cookie header 
+    res.cookie("token", signedToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true
+    })
 
-    //create New user document
-    const newUserDoc = new UserModel(newUser);
+    // remove the password field from the user obj
+    const userObj = user.toObject();
+    delete userObj.password;
 
-    //save document
-    await newUserDoc.save();
-    //send res
-    res.status(201).json({ message: "User created" });
-  } catch (err) {
-    console.log("err is ", err);
-    //delete image from cloudinary
-    if (cloudinaryResult.public_id) {
-      await cloudinary.uploader.destroy(cloudinaryResult.public_id);
-    }
-    next(err);
-  }
-});
+    res.status(200).json({ message: "Login Successful", payload: userObj })
+})
 
-//Route for Login(USER, AUTHOR and ADMIN)
-commonApp.post("/login", async (req, res) => {
-  //console.log(req.body)
-  //get user cred obj
-  const { email, password } = req.body;
-  //find user by email
-  const user = await UserModel.findOne({ email: email });
-  //if use not found
-  if (!user) {
-    return res.status(400).json({ message: "Invalid email" });
-  }
-  //compare password
-  const isMatched = await compare(password, user.password);
-  //if passwords not matched
-  if (!isMatched) {
-    return res.status(400).json({ message: "Invalid password" });
-  }
-  //create jwt
-  const signedToken = sign(
-    {
-      id: user._id,
-      email: email,
-      role: user.role,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      profileImageUrl: user.profileImageUrl,
-    },
-    process.env.SECRET_KEY,
-    {
-      expiresIn: "1h",
-    },
-  );
+// route for logout
+commonApp.get('/logout', (req, res) => {
+    // delete the teoken from the cookie storage
+    res.clearCookie("token", {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true
+    })
+    res.status(200).json({ message: "Logged out successfully" });
+})
 
-  //set token to res header as httpOnly cookie
-  res.cookie("token", signedToken, {
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-  });
-  //remove password from user document
-  let userObj = user.toObject();
-  delete userObj.password;
-
-  //send res
-  res.status(200).json({ message: "login success", payload: userObj });
-});
-
-//Route for Logout
-commonApp.get("/logout", (req, res) => {
-  //delete token from cookie storage
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-  });
-  //send res
-  res.status(200).json({ message: "Logout success" });
-});
-
-//Page refresh
+// Page for refresh
 commonApp.get("/check-auth", verifyToken("USER", "AUTHOR", "ADMIN"), (req, res) => {
-  res.status(200).json({
-    message: "authenticated",
-    payload: req.user,
-  });
+    res.status(200).json({
+        message: "authenticated",
+        payload: req.user,
+    });
 });
 
-//Change password
-commonApp.put("/password", verifyToken("USER", "AUTHOR", "ADMIN"), async (req, res) => {
-  //check current password and new password are same
-  //get current password of user/admin/author
-  //check the current password of req and user are not same
-  // hash new password
-  //replace current password of user with hashed new password
-  //save
-  //send res
-});
+// change the password
+commonApp.put('/password', verifyToken("ADMIN", "AUTHOR", "USER"), async (req, res) => {
+    // check if current and new Passwords are same
+    const { currentPassword, newPassword } = req.body;
+
+    if (currentPassword === newPassword) {
+        return res.json({ message: "Current and new passwords should not be the same" })
+    }
+
+    // get current password from the role loggedin
+    const userId = req.user?.id;
+
+    // find the user
+    const user = await UserModel.findById(userId);
+
+    const isMatched = await compare(currentPassword, user.password);
+
+    //chck the current paswword of logged in role and req are same/not
+    if (!isMatched) {
+        return res.status(400).json({ message: "Current password is not matching" })
+    }
+
+    //hash the password
+    const hashedPassword = await hash(newPassword, 12);
+
+    //replace the password and save
+    user.password = hashedPassword
+    user.save();
+
+    //send res
+    res.status(200).json({ message: "Password updated" })
+})
