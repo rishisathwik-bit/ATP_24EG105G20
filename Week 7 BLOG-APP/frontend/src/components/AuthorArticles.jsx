37f356e357c2d99545486d564c2f47af10fab522
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-13 21:35:28 +0530

    Update AuthorArticles.jsx

--- Week 7 BLOG-APP/frontend/src/components/AuthorArticles.jsx
+++ Week 7 BLOG-APP/frontend/src/components/AuthorArticles.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router";
-import { useAuth } from "../store/authStore";
+import { useAuth } from "../stores/authStore";
 
 import {
   articleCardClass,
@@ -24,20 +24,21 @@ function AuthorArticles() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  console.log("user in author profile", user);
-
+  console.log("user in author profile",user)
+  
   useEffect(() => {
     if (!user) return;
 
     const getAuthorArticles = async () => {
+      
+
       try {
         setLoading(true);
-        //read articles of current author
-        let res = await axios.get("https://atp-24eg105g20-2.onrender.com/author-api/articles", { withCredentials: true } );
-        if (res.status === 200) {
-          setArticles(res.data?.payload);
-        }
-        //update articles state
+       //read articles of current author
+       let res= await axios.get("https://atp-24eg105g20-2.onrender.com/author-api/articles",{withCredentials:true})
+       //update articles state
+       if(res.status===200)
+        setArticles(res.data?.payload);
       } catch (err) {
         console.log(err);
         setError(err.response?.data?.error || "Failed to fetch articles");
@@ -50,7 +51,7 @@ function AuthorArticles() {
   }, [user]);
 
   const openArticle = (article) => {
-    navigate(`/articles/${article._id}`, {
+    navigate(`/article/${article._id}`, {
       state: article,
     });
   };
