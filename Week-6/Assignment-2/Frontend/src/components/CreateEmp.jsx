e4e421cd9a35156b788a059d8cef200bbe2e182d
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-15 14:23:55 +0530

    Update CreateEmp.jsx

--- Week-6/Assignment-2/Frontend/src/components/CreateEmp.jsx
+++ Week-6/Assignment-2/Frontend/src/components/CreateEmp.jsx
@@ -20,7 +20,7 @@ function CreateEmp() {
     try {
       setLoading(true);
       //make HTTP POST req
-      let res = await fetch("http://localhost:4000/emp-api/employees", {
+      let res = await fetch("https://employee-reactapp.onrender.com/emp-api/employees", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newEmpObj),
