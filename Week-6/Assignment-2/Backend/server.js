2b76c9a265faf762ed95df8a5c07a87d60b131f6
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-15 13:38:42 +0530

    Refactor server.js for improved structure and API

--- Week-6/Assignment-2/Backend/server.js
+++ Week-6/Assignment-2/Backend/server.js
@@ -1,34 +1,32 @@
-import exp from 'express'
-import { connect} from 'mongoose'
-import cors from 'cors'
+import exp from 'express';
+import { empApp } from './APIs/EmployeeAPI.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
+const app = exp();
 
-const app=exp();
-//add cors middleware
-app.use(cors({
-    origin:["http://localhost:5173"],
-}),
-);
-// body parser middleware
+app.use(cors());
 app.use(exp.json());
-//emp api middleware
 
+app.use('/emp-api', empApp);
 
-//db connection
-const connectDB=async()=>{
-    try{
-        await connect("mongodb://localhost:27017/empdb");
-       console.log("DB Connected Successfull");
-       app.listen(4000,()=>console.log("Server listening on port 4000..."));
-    }catch(err){
-        console.log("err in db connection ",err.message);
-    }
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-};
+    const PORT = process.env.PORT || 4000;
+
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.log("error occured while connecting to dbase", err);
+  }
+}
 
 connectDB();
 
-//error handling middleware
 app.use((err, req, res, next) => {
   console.log("err in middleware:", err.message);
 
