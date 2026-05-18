4ae89232dd2736c778dab76b8ff58c0a0fd1256a
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-18 19:03:51 +0530

    Update WriteArticles.jsx

--- Week-7 BlogApp/frontend/src/components/WriteArticles.jsx
+++ Week-7 BlogApp/frontend/src/components/WriteArticles.jsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form'
 import { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
-import { useNavigate } from 'react-router'
+import { useNavigate } from 'react-router-dom'
 import BASE_URL from '../config'
 import {
   formCard,
