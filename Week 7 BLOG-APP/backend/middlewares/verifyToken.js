49014409a52ecd4a6a4039eb10ef8d698ace5b54
rishisathwik-bit <rishisathwik87@gmail.com>
2026-05-19 11:32:04 +0530

    Update verifyToken.js

--- Week 7 BLOG-APP/backend/middlewares/verifyToken.js
+++ Week 7 BLOG-APP/backend/middlewares/verifyToken.js
@@ -6,30 +6,46 @@ config();
 export const verifyToken = (...allowedRoles) => {
   return (req, res, next) => {
     try {
-      //get token from cookie
-      const token = req.cookies?.token; // { token : asdasd}
-      //check token existed or not
-      if (!token) {
-        return res.status(401).json({ message: "Please login first" });
-      }
-      //validate token(decode the token)
-      let decodedToken = verify(token, process.env.SECRET_KEY);
 
-      // check the role is same as role in decodedToken
-      if (!allowedRoles.includes(decodedToken.role)) {
-        return res.status(403).json({ message: "You are not authorized" });
+      console.log("Cookies:", req.cookies);
+
+      const token = req.cookies?.token;
+
+      console.log("Token:", token);
+
+      if (!token) {
+        return res.status(401).json({
+          message: "Please login first"
+        });
       }
-      //add decoded token
+
+      let decodedToken = verify(
+        token,
+        process.env.SECRET_KEY
+      );
+
+      console.log("Decoded:", decodedToken);
+
+      if (!allowedRoles.includes(decodedToken.role)) {
+        return res.status(403).json({
+          message: "You are not authorized"
+        });
+      }
+
       req.user = decodedToken;
+
       next();
+
     } catch (err) {
-      res.status(401).json({ message: "Invalid token" });
+
+      console.log(err);
+
+      res.status(401).json({
+        message: "Invalid token"
+      });
     }
   };
 };
-
-
-
 
 
 
